90accefca38d1645e104e30c0345a950cb091fc0
Thiago Marinho <tgmarinho@gmail.com>
2020-04-11 21:18:42 -0400

    refactoring for pass in the test

--- src/App.js
+++ src/App.js
@@ -4,17 +4,10 @@ import "./styles.css";
 
 function App() {
   const [repositories, setRepositories] = useState([]);
-  const [title, setTitle] = useState("");
-  const [url, setUrl] = useState("");
-  const [tech, setTech] = useState("");
-  const [techs, setTechs] = useState([]);
-  const [loading, setLoading] = useState(false);
 
   const loadRepositories = async () => {
-    setLoading(true);
     const { data } = await api.get("/repositories");
-    console.log(data);
-    setLoading(false);
+
     setRepositories(data);
   };
 
@@ -22,51 +15,36 @@ function App() {
     loadRepositories();
   }, []);
 
-  async function handleAddRepository(e) {
-    e.preventDefault();
-
-    api.post("/repositories", { title, url, techs });
-    loadRepositories();
+  async function handleAddRepository() {
+    const response = await api.post("/repositories", {
+      title: "CArla",
+      url: "http://tgmarinho.com",
+      techs: ["React", "React Native"],
+    });
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
     await api.delete(`/repositories/${id}`);
-    console.log("testing rodando");
-    loadRepositories();
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
-  function handleAddTech(e) {
-    if (!tech) return;
-    setTechs([...techs, tech]);
-    setTech("");
-  }
-
-  return !loading ? (
+  return (
     <div>
       <ul data-testid="repository-list">
-        {!!repositories.length &&
-          repositories.map((repository) => (
-            <li key={repository.id}>
-              {repository.title}
-              <button onClick={() => handleRemoveRepository(repository.id)}>
-                Remover
-              </button>
-            </li>
-          ))}
+        {repositories.map((repository) => (
+          <li key={repository.title}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-      <br />
-      Title: <input value={title} onChange={(e) => setTitle(e.target.value)} />
-      <br />
-      Url: <input value={url} onChange={(e) => setUrl(e.target.value)} />
-      <br />
-      Tech: <input value={tech} onChange={(e) => setTech(e.target.value)} />
-      <button onClick={handleAddTech}>Adicionar Tech</button>
-      <p>{techs.join(", ")}</p>
-      <br />
+
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
-  ) : (
-    <p>Carregando...</p>
   );
 }
 
